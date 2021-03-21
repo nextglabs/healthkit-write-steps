ba3c2ce3c5c2a28b3c9654bb7bacf8ea07cfb9f0
Bassem <bassem@coradine.com>
2021-03-21 19:56:19 +0100

    Initial Commit

---
+++ StepsWrite/StepsWriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepsWriteApp.swift
+//  StepsWrite
+//
+//  Created by Bassem (NextGLabs) on 21/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct StepsWriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
